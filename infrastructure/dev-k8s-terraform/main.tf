e62dba890b1dcc86660048e53a850c900689e314
okermen <oguzhankermen@gmail.com>
2023-06-05 18:16:53 +0000

    added dev-k8s-terraform  for kubernetes infrastructure

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -126,8 +126,8 @@ resource "aws_security_group" "petclinic-kube-master-sg" {
   }
 }
 
-resource "aws_iam_role" "petclinic-master-server-s3-role" {
-  name               = "petclinic-master-server-role"
+resource "aws_iam_role" "petclinic-master-server-s3role" {
+  name               = "petclinic-master-serverrole"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -147,8 +147,8 @@ EOF
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"]
 }
 
-resource "aws_iam_instance_profile" "petclinic-master-server-profile" {
-  name = "petclinic-master-server-profile"
+resource "aws_iam_instance_profile" "petclinic-master-serverprofile" {
+  name = "petclinic-master-serverprofile"
   role = aws_iam_role.petclinic-master-server-s3-role.name
 }
 
